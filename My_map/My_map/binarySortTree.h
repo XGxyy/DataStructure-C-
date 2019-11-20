2df7b8266e5f0e41f876d39f1debac713c6e88a3
XGxyy <574801937@qq.com>
2019-11-20 21:19:36 +0800

    1120的补充

--- My_map/My_map/binarySortTree.h
+++ My_map/My_map/binarySortTree.h
@@ -56,6 +56,7 @@ public:
 	}
 
 	bool erase(const T& val){
+#if 0
 		if (m_root == nullptr){
 			m_root = new TreeNode<T>(val);
 			return true;
@@ -78,7 +79,7 @@ public:
 		}
 		
 		if (cur == nullptr){
-			return false;
+		return false;
 		}
 
 		if (cur->m_left && cur->m_right){
@@ -101,7 +102,88 @@ public:
 			}
 			delete cur;
 		}
-	
+#else
+		if (m_root == nullptr){
+			return false;
+		}
+
+		TreeNode<T> * cur = m_root;
+		TreeNode<T> * pre = nullptr;
+
+		while (cur){
+			if (val < cur->m_data){
+				pre = cur;
+				cur = cur->m_left;
+			}
+
+			else if (val > cur->m_data){
+				pre = cur;
+				cur = cur->m_right;
+			}
+
+			else{
+				break;
+			}
+		}
+
+		if (cur == nullptr){
+			return false;
+		}
+		if (cur->m_left && cur->m_right){
+			TreeNode<T> * cur2 = cur->m_left;
+			TreeNode<T> * pre2 = cur;
+			
+			if (cur2->m_right){
+				for (; cur2->m_right; pre2 = cur2, cur2 = cur2->m_right);
+				pre2->m_right = cur2->m_left;
+				cur2->m_left = cur->m_left;
+			}
+			cur2->m_right = cur->m_right;
+			if (cur == pre){
+
+			}
+			if (cur->m_data < pre->m_data){
+				pre->m_left = cur2;
+			}
+
+			else{
+				pre->m_right = cur2;
+			}
+			delete cur;
+		}
+
+		else if (cur->m_left){
+			if (cur == pre){
+				m_root = cur->m_left;
+			}
+			else{
+				if (cur->m_data < pre->m_data){
+					pre->m_left = cur->m_left;
+				}
+
+				else{
+					pre->m_right = cur->m_left;
+				}
+			}
+			delete cur;
+		}
+
+		else{
+			if (cur == pre){
+				m_root = cur->m_left;
+			}
+			else{
+				if (cur->m_data < pre->m_data){
+					pre->m_left = cur->m_right;
+				}
+
+				else{
+					pre->m_right = cur->m_right;
+				}
+			}
+			delete cur;
+		}
+#endif
 	}
 };
 }
